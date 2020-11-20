04d44cd11b65fff81cde4beb4ed34148e2060849
mtatanashvili <mt@fuf.de>
2020-11-20 11:41:04 +0100

    Fixing Completed items saving
    Fixing Completed items saving

--- js/script.js
+++ js/script.js
@@ -3,14 +3,19 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const form = document.querySelector('form');
 let idForNewElement = getRandomInt(1, 10)
+const showDoneItems = document.querySelector('.done-items');
+let doneItems = 0;
 getData();
+
+
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   let myItem = input.value;
   saveinDb(myItem)
-  createItem(myItem , idForNewElement)
+  createItem(myItem, idForNewElement, 'open')
 
 })
 
@@ -28,29 +33,31 @@ function getData() {
           data.forEach(element => {
             const dataText = element.text
             const dataId = element.id
+            const dataStatus = element.status
             //   console.log('id-check')
-            //   console.log(itemId)
-            createItem(dataText,dataId)
+            //   console.log(element.status)
+              createItem(dataText,dataId,dataStatus)
           });
 
         });
       })
-        .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-        });
+    .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+    })
+
 }
 
 
 
 //Get all items from database
-  function createItem(itemText , itemId) {
+  function createItem(itemText , itemId , itemStatus) {
     if(itemText != 0) {
       const listItem = document.createElement('li');
       const listText = document.createElement('span');
       const deleteBtn = document.createElement('button');
       const checkBox = document.createElement('input')
-      const showItems = document.querySelector('.done-items')
-      let doneItems = 0;
+      
+      
       checkBox.setAttribute('type','checkbox');
       checkBox.setAttribute('class','item-checkbox');
       listItem.appendChild(checkBox);
@@ -60,24 +67,47 @@ function getData() {
       
       listItem.appendChild(deleteBtn);
       listItem.setAttribute('data-id', itemId)
+      listItem.setAttribute('data-id', itemId)
+      listItem.setAttribute('data-status', itemStatus)
       deleteBtn.textContent = 'Delete';
     //   console.log('id-check')
     //   console.log(itemId)
       const itemgetId = listItem.getAttribute('data-id')
+      if(itemStatus === 'completed') {
+        listItem.classList.add('completed')
+        checkBox.checked = true;
+        doneItems ++;
+      }
       ul.appendChild(listItem);
-    
+      //console.log(document.querySelectorAll('.my-items .completed').length);
+      showItems();
       deleteBtn.addEventListener('click', () => {
     //   console.log('id-check')
     //   console.log(itemgetId) 
-        removeItem(itemgetId)  
-        ul.removeChild(listItem);
-        doneItems = document.querySelectorAll('.my-items .striked').length;
-        showItems.innerText = doneItems;
+       
+        ul.removeChild(listItem);        
+        removeItem(itemgetId)
+        showItems();
       })
       checkBox.addEventListener('change',() => {
-          checkBox.parentElement.classList.toggle('striked');
-          doneItems = document.querySelectorAll('.my-items .striked').length;
-          showItems.innerText = doneItems;
+          const itemElem = checkBox.parentElement;
+          const elId = itemElem.getAttribute('data-id')
+          let elStatus = itemElem.getAttribute('data-status')
+          itemElem.classList.toggle('completed');
+          showItems();
+
+          if(elStatus === 'open') {
+            itemElem.setAttribute('data-status','completed')
+            updateItem(elId, 'completed')
+          } else if(elStatus === 'completed') {
+            itemElem.setAttribute('data-status','open')
+            updateItem(elId, 'open')
+          }
+
+          console.log(elStatus)
+          //saveOrUpdateDb()
+          
+
       }) 
       input.value = '';
       input.focus();
@@ -86,7 +116,7 @@ function getData() {
 
 
 function saveinDb(dataText) {
-const data = { text: dataText };
+const data = { text: dataText ,status: 'open' };
 if(data != 0) {
 fetch('http://localhost:3000/posts', {
   method: 'POST', // or 'PUT'
@@ -114,6 +144,21 @@ fetch(`http://localhost:3000/posts/${id}`, {
 .then(res => console.log(res))
 }
 
+function updateItem(id, itemStatus) {
+    fetch(`http://localhost:3000/posts/${id}`, {
+    method: "PATCH",
+    headers: {
+        "Content-Type" : "application/json"
+      },
+    body: JSON.stringify(
+      {
+        "status": itemStatus 
+      }
+    )
+  });
+}
+
+
 
 /**
  * Return a random Int Num between min (inclusive) and max (exclusive)
@@ -122,4 +167,12 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Calculate completed items
+function showItems() {
+    let doneItems = document.querySelectorAll('.my-items .completed').length;
+    //console.log(doneItems)
+    showDoneItems.innerText = doneItems;
+    // return doneItems
 }
